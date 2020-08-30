81ae74617fa292e7dd47eb9d361d7ed52d63e399
Dmitry Karkin <dmkarkin@gmail.com>
2020-08-30 09:47:07 +0300

    small changes

--- gulp/config.js
+++ gulp/config.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require('path');
 
-const root = path.join(__dirname, '../')
-const src = path.join(root, 'src')
+const root = path.join(__dirname, '../');
+const src = path.join(root, 'src');
 
 module.exports = {
   root,
